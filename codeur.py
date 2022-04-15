0a9a9ad86d7029a3cce7851043e0062f13fc2c6c
LordWilose <ecotierel@gmail.com>
2022-04-15 07:55:03 +0200

    v4.2
    Supression des modifications pour la GUI.
    Ajout d'un 6è bit de cryptage pour la factorisation.

--- codeur.py
+++ codeur.py
@@ -7,16 +7,12 @@
 #																			  #
 ############################################################################### 
 
-from random import randrange
-import tkinter as tk
+from random import randrange, choice
 
-################################### CLASS ################################
-# Recupération entrée
-class EncryptThis():
+################################### CST #####################################
+# Table morse
 
-	def __init__(self, text, verbose=False, tkinter=False):
-		# Table morse
-		self.morse_dict = {"a": "120000", "b": "211100", "c": "212100", "d": "211000", "e": "100000",
+morse_dict = {"a": "120000", "b": "211100", "c": "212100", "d": "211000", "e": "100000",
 			  "f": "112100", "g": "221000", "h": "111100", "i": "110000", "j": "122200",
 			  "k": "212000", "l": "121100", "m": "220000", "n": "210000", "o": "222000",
 			  "p": "122100", "q": "221200", "r": "121000", "s": "111000", "t": "200000",
@@ -27,227 +23,270 @@ class EncryptThis():
 			  "9": "222210", "0": "222220", ",": "221122", ".": "121212", "?": "112211",
 			  '"': "121121", ":": "222111", "'": "122221", "-": "211112", "/": "211210",
 			  "(": "212210", ")": "212212", " ": "000000"}
-		self.text = text
-		self.verbose = verbose
-		self.tkinter = tkinter
-		print(tkinter)
 
+################################### FONCTION ################################
+# Recupération entrée
+def splitAndConvert(text):
+	words = []
+	word = ""
 
-	def run(self):
-		# Récupération de l'entrée et découpage en séquence de mot
+	for letter in text:
+		word += letter.lower()
+		if letter == " ":
+			# Reset
+			words.append(word)
+			word = ""
 
-		self.splitAndConvert()
+	return words
 
-		# Conversion en morse
+# Conversion alpha->morse (6bits : a-z + 0-9 + àù... + (,?;.)... )
+def strToMorse(words):
+	morse_words = []
 
-		self.strToMorse()
+	for word in words:
+		morse_word = ""
 
-		# Cryptage du morse
+		for letter in word:
+			# Conversion decimal vers string
+			morse_letter = str(morse_dict[letter])
+			morse_word += morse_letter
 
-		self.cryptIt()
+		morse_words.append(morse_word)
 
-		# Conversion en binaire sur 1 octet (4*2bits 5*2 si n° avec)
-		#	00 = pas de signal
-		#	10 = signal court
-		#	11 = signal long
+	return morse_words
 
-		self.morseToBin()
+# Conversion morse->bin
+def morseToBin(morse_words):
+	bin_words = []
 
-		# Conversion en hexadecimal par octet (de morse binaire)
+	for word in morse_words:
+		bin_word = ""
+		rank, nMorse = 0, 0
 
-		self.binToHexa()
+		while nMorse < len(word)-1:
 
-		# Traduction dans la table ascii
-		#	(à étendre utf-8 ?)
+			for i in range(6):
+				nMorse = i+rank
+				morse = word[nMorse]
+				
+				if morse == "0":
+					bin_word += "00"
+				elif morse == "1":
+					bin_word += "10"
+				else: #2
+					bin_word += "11"
 
-		self.hexToAscii()
+			rank += 6
+		bin_words.append(bin_word)
 
-		if self.verbose:
-			self.beVerbose()
+	return bin_words
 
+# Conversion bin->hexa
+def binToHexa(bin_words):
+	hex_words = []
 
-	def splitAndConvert(self):
-		self.words = []
-		word = ""
+	for word in bin_words:
+		hex_word = ""
+		nHex, rank = 0, 0
 
-		for letter in self.text:
-			word += letter.lower()
-			if letter == " ":
-				# Reset
-				self.words.append(word)
-				word = ""
+		while nHex < len(word)-1:
+			tmp_hex = ""
 
+			for i in range(12):
+				try:
+					nHex = rank+i
+					tmp_hex += word[nHex]
+				except IndexError:
+					pass
 
-	# Conversion alpha->morse (6bits : a-z + 0-9 + àù... + (,?;.)... )
-	def strToMorse(self):
-		self.morse_words = []
+			rank += 12
+			hex_number = hex(int(tmp_hex, 2)).replace("0x", "")
 
-		for word in self.words:
-			morse_word = ""
+			while len(hex_number) < 3:
+				hex_number = "0"+hex_number
 
-			for letter in word:
-				# Conversion decimal vers string
-				morse_letter = str(self.morse_dict[letter])
-				morse_word += morse_letter
+			hex_word += hex_number	
 
-			self.morse_words.append(morse_word)
+		hex_words.append(hex_word)
 
+	return hex_words
 
-	# Conversion morse->bin
-	def morseToBin(self):
-		self.bin_words = []
+# Conversion hex->ascii
+def hexToAscii(hex_words):
+	ascii_words = []
 
-		for word in self.morse_words:
-			bin_word = ""
-			rank, nMorse = 0, 0
+	for word in hex_words:
+		ascii_word = ""
+		nHex, rank = 0, 0
 
-			while nMorse < len(word)-1:
+		while nHex < len(word)-1:
+			tmp = ""
 
-				for i in range(6):
-					nMorse = i+rank
-					morse = word[nMorse]
-					
-					if morse == "0":
-						bin_word += "00"
-					elif morse == "1":
-						bin_word += "10"
-					else: #2
-						bin_word += "11"
+			for i in range(3):
+				nHex = i+rank
+				tmp += word[nHex]
 
-				rank += 6
-			self.bin_words.append(bin_word)
+			rank += 3
+			char = chr(int(tmp, base=16)+150) # Ajout de la constante 150 pour éviter les contrôles qui ne sont pas détectables
+			ascii_word += char
 
+		ascii_words.append(ascii_word)
 
-	# Conversion bin->hexa
-	def binToHexa(self):
-		self.hex_words = []
+	return ascii_words
 
-		for word in self.bin_words:
-			hex_word = ""
-			nHex, rank = 0, 0
+def cryptIt(raw, verbose=False):
+	new_morse_words = []
 
-			while nHex < len(word)-1:
-				tmp_hex = ""
+	for word in raw:
 
-				for i in range(12):
-					try:
-						nHex = rank+i
-						tmp_hex += word[nHex]
-					except IndexError:
-						pass
+		# Randomisation bits 1/2/3/4/5
+		# 1 échange entre valeurs 0 {x,y,z}/ 1 {z,x,y} / 2 {y,z,x}
+		# 2 Décalage d'indice
+		# 3 Détermine la cible de l'échange x du 1er bit
+		# 4 Idem pour y
+		# 5 Idem pour z
+		# 6 X*len(mot) -> padding de taille, mot = x premiers bits
 
-				rank += 12
-				hex_number = hex(int(tmp_hex, 2)).replace("0x", "")
+		bit1 = randrange(0,3) # 0, 1, 2
+		bit2 = randrange(0,3)
+		bit3 = randrange(0,3)
+		bit4 = randrange(0,3)
+		bit5 = randrange(0,3)
+		bit6 = randrange(0,3)
 
-				while len(hex_number) < 3:
-					hex_number = "0"+hex_number
-
-				hex_word += hex_number	
-
-			self.hex_words.append(hex_word)
-
-
-	# Conversion hex->ascii
-	def hexToAscii(self):
-		self.ascii_words = []
-
-		for word in self.hex_words:
-			ascii_word = ""
-			nHex, rank = 0, 0
-
-			while nHex < len(word)-1:
-				tmp = ""
-
-				for i in range(3):
-					nHex = i+rank
-					tmp += word[nHex]
-
-				rank += 3
-				char = chr(int(tmp, base=16)+150) # Ajout de la constante 150 pour éviter les contrôles qui ne sont pas détectables
-				ascii_word += char
-
-			self.ascii_words.append(ascii_word)
-
-
-	def cryptIt(self):
-		self.crypted_morse_words = []
-
-		for word in self.morse_words:
-
-			# Randomisation bits 1/2/3/4/5
-			# 1 échange entre valeurs 0 {x,y,z}/ 1 {z,x,y} / 2 {y,z,x}
-			# 2 Décalage d'indice
-			# 3 Détermine la cible de l'échange x du 1er bit
-			# 4 Idem pour y
-			# 5 Idem pour z
-
-			bit1 = randrange(0,3) # 0, 1, 2
-			bit2 = randrange(0,3)
+		while bit3 == bit4 or bit3 == bit5 or bit4 == bit5:
+			# Evite la redondance dans l'échange de valeurs
 			bit3 = randrange(0,3)
 			bit4 = randrange(0,3)
 			bit5 = randrange(0,3)
-			bit6 = 0 # Padding (%6)
 
-			while bit3 == bit4 or bit3 == bit5 or bit4 == bit5:
-				# Evite la redondance dans l'échange de valeurs
-				bit3 = randrange(0,3)
-				bit4 = randrange(0,3)
-				bit5 = randrange(0,3)
+		if verbose:
+			print("Start : "+word)
+			print("Bits: "+str([bit1, bit2, bit3, bit4, bit5, bit6]))
 
-			# print("Start : "+word)
-			# print("Bits: "+str([bit1, bit2, bit3, bit4, bit5, bit6]))
+		if bit1 == 0: # Destinations de la fonction d'échange de valeurs
+			dsts_change = [bit3, bit4, bit5]
+		elif bit1 == 1:
+			dsts_change = [bit5, bit3, bit4]
+		else: # 2
+			dsts_change = [bit4, bit5, bit3]
 
-			if bit1 == 0:
-				dsts_change = [bit3, bit4, bit5]
-			elif bit1 == 1:
-				dsts_change = [bit5, bit3, bit4]
-			else: # 2
-				dsts_change = [bit4, bit5, bit3]
+		if verbose:
+			print("Dsts : "+str(dsts_change))
 
-			# print("Dsts : "+str(dsts_change))
+		new_word = "" # Execution de l'echange
+		for char in word:
+			new_word += str(dsts_change[int(char)])
 
-			new_word = ""
-			for char in word:
-				new_word += str(dsts_change[int(char)])
+		if verbose:
+			print("Change : "+new_word)
 
-			# print("Change : "+new_word)
+		if bit2 == 0: # Remise à niveau du décalage modulo
+			pass
+		elif bit2 == 1:
+			lastChar = new_word[len(new_word)-1]
+			new_word = lastChar+new_word
+			new_word = new_word[:-1]
+		else: # 2
+			firstChar = new_word[0]
+			new_word = new_word+firstChar
+			new_word = new_word[1:]
 
-			if bit2 == 0:
-				pass
-			elif bit2 == 1:
-				lastChar = new_word[len(new_word)-1]
-				new_word = lastChar+new_word
-				new_word = new_word[:-1]
-			else: # 2
-				firstChar = new_word[0]
-				new_word = new_word+firstChar
-				new_word = new_word[1:]
+		if verbose:
+			print("Decalage : "+new_word)
 
-			# print("Decalage : "+new_word+"\n\n")
+		tmp = [] # Ajout de factorisation du mot 
+		for x in range(bit6):
+			l = 0
+			tmp_tmp = ""
 
-			new_word = str(bit1)+str(bit2)+str(bit3)+str(bit4)+str(bit5)+str(bit6)+new_word
-			self.crypted_morse_words.append(new_word)
+			while l < len(new_word)+6:
+				tmp_tmp += choice(["0", "1", "2"])
+				l += 1
 
+			tmp.append(tmp_tmp)
 
-	def beVerbose(self):
-		if self.tkinter:
+		padding = "".join(tmp)
 
-			self.root = tk.Tk()
-			self.root.title("ENCRYPTED")
-			i = 2
-			for wordlist in [self.morse_words, self.crypted_morse_words,
-						 self.bin_words, self.hex_words, self.ascii_words]:
-				for word in wordlist:
-					tk.Label(self.root, text=str(word)).grid(row=i, column=2)
-					i+=1
-				i+=1
-			self.root.mainloop()
+		if verbose:
+			print("Padding : "+padding+"\n")
+
+		new_word = str(bit1)+str(bit2)+str(bit3)+str(bit4)+str(bit5)+str(bit6)+new_word+padding
+
+		if verbose:
+			print("Final : "+new_word+"\n\n")
 		
-		else:
-			for wordlist in [self.morse_words, self.crypted_morse_words,
-							 self.bin_words, self.hex_words, self.ascii_words]:
-				for word in wordlist:
-					print(str(len(word))+" "+word)
-				print("\n")
-			print("Sortie : %s"%(" ".join(self.ascii_words)))
+		new_morse_words.append(new_word)
 
+	return new_morse_words, [bit1, bit2, bit3, bit4, bit5, bit6]
+
+
+################################### MAIN ####################################
+# Récupération de l'entrée et découpage en séquence de mot
+
+text = input("Texte à encoder\n/!\\ Peu de caractères spéciaux sont pris en charge /!\\\n: ") + " "
+
+beVerbose = input("Bavard ? (y/N) : ")
+if beVerbose in ("y", "Y", "yes", "Yes", "YES", "ye", "Ye", "YE"):
+	beVerbose = True
+else:
+	beVerbose = False
+
+words = splitAndConvert(text)
+
+if beVerbose:
+	for word in words:
+		print(str(len(word))+" "+word)
+	print("\n")
+
+# Conversion en morse
+
+morse_words = strToMorse(words)
+
+if beVerbose:
+	for word in morse_words:
+		print(str(len(word))+" "+word)
+	print("\n")
+
+morse_words, encryptionBits = cryptIt(morse_words, beVerbose)
+
+if beVerbose:
+	for word in morse_words:
+		print(str(len(word))+" "+word)
+	print("\n")
+
+# Conversion en binaire sur 1 octet (4*2bits 5*2 si n° avec)
+#	00 = pas de signal
+#	10 = signal court
+#	11 = signal long
+
+bin_words = morseToBin(morse_words)
+
+if beVerbose:
+	for word in bin_words:
+		print(str(len(word))+" "+word)
+	print("\n")
+
+# Conversion en hexadecimal par octet (de morse binaire)
+
+hex_words = binToHexa(bin_words)
+
+if beVerbose:
+	for word in hex_words:
+		print(str(len(word))+" "+word)
+	print("\n")
+
+
+# Traduction dans la table ascii
+#	(à étendre utf-8 ?)
+
+ascii_words = hexToAscii(hex_words)
+
+if beVerbose:
+	for word in ascii_words:
+		print(str(len(word))+" "+word)
+	print("\n")
+
+# Compilation de la sortie
+print("################# SORTIE #################\n")
+print(" ".join(ascii_words))
